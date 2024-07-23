76f912f89a69dfb369c48f6b3a71246303527ee1
Rishabh <rishabhshah2005@gmail.com>
2024-07-23 20:56:44 +0530

    first commit

---
+++ src/sandbox.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class sandbox {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        
+        String dburl = "jdbc:mysql://localhost:3306/payment";
+        String dbuser = "root";
+        String dbpass = "";
+
+        Connection con = DriverManager.getConnection(dburl, dbuser, dbpass);
+
+        if (con!=null) {
+            String sql = "Select * from users";
+            PreparedStatement pst = con.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(rs.getString(2)+rs.getString(3));
+            }
+        } else {
+            System.out.println("Connection failed");
+        }
+    }
+}
